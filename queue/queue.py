8c642854505da361bd7b91ccbd1f80578ecdcc91
Hargobind Atwal <ethaipath@gmail.com>
2019-02-25 10:01:20 -0800

    Started creating a doubly linked list

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,34 @@
+class link:
+  def __init__(self, data, nextNode, prev):
+    self.data = data
+    self.next = nextNode
+    self.prev = prev
+
 class Queue:
   def __init__(self):
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = 
+    # a doubly linked list
+    self.head = None
+    self.tail = None
+    self.length = 0
 
-  def enqueue(self, item):
+  def enqueue(self, link):
+    # set link 
+    link.prev = self.HEAD
+    self.head.next = link
+    self.head = link
+    self.length += 1
     pass
   
   def dequeue(self):
+    value = self.TAIL.data
+    self.TAIL = self.TAIL.prev
+    self.length -= 1
+    return value
     pass
 
   def len(self):
+    return self.length
     pass
