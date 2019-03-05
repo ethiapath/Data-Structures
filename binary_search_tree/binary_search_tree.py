53f0f8219fdeb0778d3ad85a0a3ddcb18310cec4
Hargobind Atwal <ethaipath@gmail.com>
2019-03-05 14:56:34 -0800

    working on contains

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,29 +5,42 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    if value <= self.value:
-      if self.left == None:
-        self.left = BinarySearchTree(value)
-      else:
-        self.left.insert(value)
-    else:
+    print('inserting:', value)
+    if value >= self.value:
       if self.right == None:
         self.right = BinarySearchTree(value)
       else:
         self.right.insert(value)
+    else:
+      if self.left == None:
+        self.left = BinarySearchTree(value)
+      else:
+        self.left.insert(value)
 
   def contains(self, target):
     if self.value == target:
+      print('found', target)
       return True
 
-    if target <= self.value:
-      if self.left == None:
-        return False
-      self.left.contains(target)
-    else:
+    print('contain rec', self.value, 'target', target, 'left', self.left.value, 'right', self.right.value)
+
+    if target >= self.value:
       if self.right == None:
         return False
-      self.right.contains(target)
+      else:
+        self.right.contains(target)
+    else:
+      if self.left == None:
+        return False
+      else:
+        self.left.contains(target)
+
+  def inOrder(self):
+    if self.left != None:
+      self.left.inOrder()
+    print(self.value)
+    if self.right != None:
+      self.right.inOrder()
 
   def get_max(self):
     if self.right == None:
