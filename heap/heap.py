f5c5b25febaa11188de7b256b7b4ebd4bca28ef8
Hargobind Atwal <ethaipath@gmail.com>
2019-03-06 19:53:25 -0800

    infinite loop in _sift_down

--- heap/heap.py
+++ heap/heap.py
@@ -1,62 +1,103 @@
+import time
 class Heap:
   def __init__(self):
     self.storage = []
+    self.length = 0
 
   def insert(self, value):
     self.storage.append(value)
-    self._bubble_up(self._get_length)
+    self.length += 1
+    self._bubble_up(len(self.storage)-1)
 
   def delete(self):
+    print('start delete')
+    time.sleep(0.5)
     root = self.storage[0]
-    self.storage[0] = self.storage[self._get_length()]
-
-    pass
+    last = self.storage[len(self.storage)-1]
+    self._swap(0, len(self.storage)-1)
+    self.length -= 1
+    self.storage[0] = self.storage[len(self.storage)-1]
+    self._sift_down(0)
+    return root
 
   def get_max(self):
+    return self.storage[0]
+    '''
     # get max value
     largests_ele = self.storage[0]
     # move last value to front
     self.storage[0] = self.storage[self.get_size-1]
-
+    '''
     pass
 
   def get_size(self):
     return len(self.storage)
 
   def _bubble_up(self, index):
-    while(index > (self._get_length())//2):
+    parent = self._get_parent_index(index)
+
+    if self.storage[parent] < self.storage[index]:
+      self._swap(parent, index)
+
+    if parent <= 0:
+      return
+    else:
+      self._bubble_up(parent)
+    '''
+    parent = self._get_parent_index(index)
+    while(self._has_parent(index) and self._get_parent(index) < self.storage[index]):
       parent = self._get_parent_index(index)
-      if (parent < self._get_length() and self.storage[index]>self.storage[parent]):
-        # swap
-        self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index] 
-        index = parent
-      else:
-        break
+      self._swap(index, self._get_parent_index(index))
+      # self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index] 
+      index = parent
+    '''
 
 
   def _sift_down(self, index):
-    top = self.storage[0]
-    larger_child
-    while index < self.get_max:
+    print('index:', index, 'value:', self.storage[index], 'heap:', self.storage)
+    time.sleep(0.5)
+    child = self._get_left_index(index)
 
-      left_child = self._get_left_index(index)
-      right_child = self._get_right_index(index)
+    if child > len(self.storage)-1:
+      return
+    # if right child exists then if it's greater set it as child
+    left = True
+    if (child + 1 <= len(self.storage)-1) and (self.storage[child+1] > self.storage[child]):
+      child += 1
+    
+    if not left:
+      print('go right')
+    else:
+      print('go left')
 
-      if (right_child < self.get and self.storage[left_child] < self.storage[right_child])
-        larger_child = right_child
+    if self.storage[index] < self.storage[child]:
+      self._swap(index, child)
+      self._sift_down(child)
+    else:
+      return
+
+    '''
+    while(self._has_left(index)):
+      if self._get_right_child(index) > self._get_left_child(index):
+        larger_child = self._get_right_index(index)
       else:
-        larger_child = left_child
-      if top >= self.storage[larger_child]:
+        larger_child = self._get_left_index(index)
+
+      if self.storage[index] > self.storage[larger_child]:
+        return
         break
+      else:
+        self._swap(index, larger_child)
+        index = larger_child
+    '''
 
-      self.storage[index] = self.storage[larger_child]
-      index = larger_child
-    self.storage[index] = top
+  def _swap(self, a, b): # a and b are indexes
+    temp = self.storage[a]
+    self.storage[a] = self.storage[b]
+    self.storage[b] = temp
 
-    pass
-
-  def _get_length(self, index):
-    return len(self.storage)-1
+  def _get_length(self):
+    return self.length#len(self.storage)-1
 
   def _get_left_index(self, index):
     return (index*2)+1
@@ -65,5 +106,22 @@ class Heap:
     return (index*2)+2
 
   def _get_parent_index(self, index):
-    return index-1//2
+    return (index-1)//2
 
+  def _has_left(self, index):
+    return self._get_left_index(index) < self.length
+
+  def _has_right(self, index):
+    return self._get_right_index(index) < self.length
+
+  def _has_parent(self, index):
+    return self._get_parent_index(index) >= 0
+
+  def _get_left_child(self, index):
+    return self.storage[self._get_left_index(index)]
+
+  def _get_right_child(self, index):
+    return self.storage[self._get_right_index(index)]
+
+  def _get_parent(self, index):
+    return self.storage[self._get_parent_index(index)]
